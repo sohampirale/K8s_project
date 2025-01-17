aeda507bc4cb5c21f6693e611ec8812d2da3e96f
wildwarrior_44 <desaivishal4904@gmail.com>
2025-01-18 03:04:39 +0530

    Initial Commit

---
+++ Backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+await mongoose.connect(process.env.MONGODB_URI);
+
+const Schema = new mongoose.Schema({
+    user1: String,
+    user2: String,
+    whoWon: String,
+    time: { type: Date, default: Date.now },
+});
+
+const Records = mongoose.model("records", Schema);
+
+// app.get("/", (req, res) => {
+//     res.send("<h1>Hello V</h1>");
+// });
+
+app.post("/nemo", async (req, res) => {
+    try {
+        const data = req.body;
+        const newScore = new Records(data);
+        await newScore.save();
+        res.status(200).json({ message: "Score saved successfully!" });
+    } catch (error) {
+        res.status(500).json({ error: "The database said go away" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server is running on some port why should I tell you?");
+});
